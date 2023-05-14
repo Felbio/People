afb541a05c4170751ef8836bee5a0f75d5f107a4
Felipe Oliveira <felipe.oliveira@walljobs.com.br>
2023-05-14 12:54:42 -0300

    Configuration of Person Controller and Service

--- src/main/java/br/com/api/people/controller/PersonController.java
+++ src/main/java/br/com/api/people/controller/PersonController.java
@@ -1,10 +1,22 @@
 package br.com.api.people.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.api.people.model.PersonModel;
+import br.com.api.people.service.PersonService;
+
 @RestController
 public class PersonController {
+
+  @Autowired
+  private PersonService ps;
+
+  @GetMapping("/listar")
+  public Iterable<PersonModel>listar(){
+      return ps.listar();
+  }
    
   @GetMapping("pessoas")
   public String route(){
--- src/main/java/br/com/api/people/service/PersonService.java
+++ src/main/java/br/com/api/people/service/PersonService.java
@@ -1,9 +1,12 @@
 package br.com.api.people.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.api.people.model.PersonModel;
+import br.com.api.people.model.ResponseModel;
 import br.com.api.people.repository.PersonRepository;
 
 @Service
@@ -12,7 +15,22 @@ public class PersonService {
     @Autowired
     private PersonRepository pr;
 
+    @Autowired
+    private ResponseModel rm;
+
     public Iterable<PersonModel> listar(){
       return pr.findAll();
+    }
+
+    public ResponseEntity<?> cadastrar(PersonModel pm){
+      if(pm.getCpf().equals("")){
+        rm.setMensage("O CPF é Obrigatório!");
+        return new ResponseEntity<ResponseModel>(rm,HttpStatus.BAD_REQUEST);        
+      }else if(pm.getEmail().equals("")){
+        rm.setMensage("O email é obrigatório");
+        return new ResponseEntity<ResponseModel>(rm,HttpStatus.BAD_REQUEST); 
+      }else{
+        return new ResponseEntity<PersonModel>(pr.save(pm),HttpStatus.CREATED);
+      }
     }
 }
