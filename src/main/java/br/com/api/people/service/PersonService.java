b652cff6e115e488e1f15ddae9b7ae6cc47e9ad5
Felipe Oliveira <felipe.oliveira@walljobs.com.br>
2023-05-14 12:38:02 -0300

    Confiuration of ResponseModel and PersonService

--- src/main/java/br/com/api/people/service/PersonService.java
+++ src/main/java/br/com/api/people/service/PersonService.java
@@ -1,5 +1,18 @@
 package br.com.api.people.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.people.model.PersonModel;
+import br.com.api.people.repository.PersonRepository;
+
+@Service
 public class PersonService {
-  
+
+    @Autowired
+    private PersonRepository pr;
+
+    public Iterable<PersonModel> listar(){
+      return pr.findAll();
+    }
 }
