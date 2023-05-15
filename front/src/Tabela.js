2f61b212ca74c78e14b572ba22f0c9e73cf99a1a
Felipe Oliveira <felipe.oliveira@walljobs.com.br>
2023-05-15 14:04:31 -0300

    Created files Tabela and Formulário, and configuration fo bootstrap

---
+++ front/src/Tabela.js
@@ -0,0 +1,32 @@
+function Tabela(){
+  return(
+      <table className='table'>
+          <thead>
+              <tr>
+                <th>#</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>CPF</th>
+                <th>Idade</th>
+                <th>Email</th>
+                <th>Endereço</th>
+                <th>Selecionar</th>                
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+          </tbody>
+      </table>
+  )
+}
+
+export default Tabela
